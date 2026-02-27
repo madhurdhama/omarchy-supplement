cc0c725a78f6ec543e6d5f23a886a1808625a646
Madhur Dhama <137516469+madhurdhama@users.noreply.github.com>
2026-02-27 12:29:11 +0530

    Updated script from basecamp/omarchy

--- omarchy-manual-install.sh
+++ omarchy-manual-install.sh
@@ -3,19 +3,34 @@
 # Set install mode to online since boot.sh is used for curl installations
 export OMARCHY_ONLINE_INSTALL=true
 
-ansi_art='                 ▄▄▄                                                   
- ▄█████▄    ▄███████████▄    ▄███████   ▄███████   ▄███████   ▄█   █▄    ▄█   █▄ 
+ansi_art='                 ▄▄▄
+ ▄█████▄    ▄███████████▄    ▄███████   ▄███████   ▄███████   ▄█   █▄    ▄█   █▄
 ███   ███  ███   ███   ███  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
 ███   ███  ███   ███   ███  ███   ███  ███   ███  ███   █▀   ███   ███  ███   ███
 ███   ███  ███   ███   ███ ▄███▄▄▄███ ▄███▄▄▄██▀  ███       ▄███▄▄▄███▄ ███▄▄▄███
 ███   ███  ███   ███   ███ ▀███▀▀▀███ ▀███▀▀▀▀    ███      ▀▀███▀▀▀███  ▀▀▀▀▀▀███
 ███   ███  ███   ███   ███  ███   ███ ██████████  ███   █▄   ███   ███  ▄██   ███
 ███   ███  ███   ███   ███  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
- ▀█████▀    ▀█   ███   █▀   ███   █▀   ███   ███  ███████▀   ███   █▀    ▀█████▀ 
+ ▀█████▀    ▀█   ███   █▀   ███   █▀   ███   ███  ███████▀   ███   █▀    ▀█████▀
                                        ███   █▀                                  '
 
 clear
 echo -e "\n$ansi_art\n"
+
+# Use custom branch if instructed, otherwise default to master
+OMARCHY_REF="${OMARCHY_REF:-master}"
+
+# Set mirror based on branch
+if [[ $OMARCHY_REF == "dev" ]]; then
+  export OMARCHY_MIRROR=edge
+  echo 'Server = https://mirror.omarchy.org/$repo/os/$arch' | sudo tee /etc/pacman.d/mirrorlist >/dev/null
+elif [[ $OMARCHY_REF == "rc" ]]; then
+  export OMARCHY_MIRROR=rc
+  echo 'Server = https://rc-mirror.omarchy.org/$repo/os/$arch' | sudo tee /etc/pacman.d/mirrorlist >/dev/null
+else
+  export OMARCHY_MIRROR=stable
+  echo 'Server = https://stable-mirror.omarchy.org/$repo/os/$arch' | sudo tee /etc/pacman.d/mirrorlist >/dev/null
+fi
 
 sudo pacman -Syu --noconfirm --needed git
 
@@ -26,17 +41,10 @@ echo -e "\nCloning Omarchy from: https://github.com/${OMARCHY_REPO}.git"
 rm -rf ~/.local/share/omarchy/
 git clone "https://github.com/${OMARCHY_REPO}.git" ~/.local/share/omarchy >/dev/null
 
-# Use custom branch if instructed, otherwise default to master
-OMARCHY_REF="${OMARCHY_REF:-master}"
-if [[ $OMARCHY_REF != "master" ]]; then
-  echo -e "\e[32mUsing branch: $OMARCHY_REF\e[0m"
-  cd ~/.local/share/omarchy
-  git fetch origin "${OMARCHY_REF}" && git checkout "${OMARCHY_REF}"
-  cd -
-fi
-
-export OMARCHY_USER_NAME="madhurdhama"
-export OMARCHY_USER_EMAIL="madhurdhama@gmail.com"
+echo -e "\e[32mUsing branch: $OMARCHY_REF\e[0m"
+cd ~/.local/share/omarchy
+git fetch origin "${OMARCHY_REF}" && git checkout "${OMARCHY_REF}"
+cd -
 
 echo -e "\nInstallation starting..."
 source ~/.local/share/omarchy/install.sh
