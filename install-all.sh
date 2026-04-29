9d8d858f9037fb7a4402dce90b227f3163370e9a
madhurdhama <madhurdhama@gamil.com>
2026-04-29 17:45:55 +0530

    sddm theme change

--- install-all.sh
+++ install-all.sh
@@ -3,4 +3,15 @@
 # Install all packages in order
 ./install-packages.sh
 ./install-dotfiles.sh
-./post-install.sh
+./omarchy-setup.sh
+
+# Disable autologin and change sddm theme
+echo -e "\e[1;33mDisable autologin and change sddm theme? (y/n):\e[0m"
+read answer
+
+if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+    echo "Running optional script..."
+    ./install-sddm.sh
+else
+    echo "Skipped"
+fi
--- omarchy-setup.sh
+++ omarchy-setup.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+omarchy-install-terminal ghostty
+omarchy-install-dev-env python
+omarchy-install-vscode
 
-# Hyprland monitor scaling
 sed -i \
   -e 's/^env = GDK_SCALE,2$/env = GDK_SCALE,1/' \
   -e 's/^monitor=,preferred,auto,auto$/monitor=,1920x1080@60,auto,1.25/' \
   ~/.config/hypr/monitors.conf
 
-# Ghostty font size
 sed -i 's/^font-size = .*/font-size = 11/' ~/.config/ghostty/config
